1cd7da8d9e05b4e47727625644a2f3ee0fca5406
Peterson de Almeida Oenning <oenningpeterson9@gmail.com>
2024-02-15 14:10:57 -0400

    Update - Seleção de itens

--- script.js
+++ script.js
@@ -7,11 +7,13 @@
         document.getElementById("state").value = "";
         document.getElementById("phone").value = "";
 
-        // Adicione mais campos, se necessário
-
-        // Limpe a seleção dos itens clicáveis
         var clicavelDivs = document.getElementsByClassName("clicavelDiv");
         for (var i = 0; i < clicavelDivs.length; i++) {
             clicavelDivs[i].classList.remove("selecionado");
         }
     }
+
+    function minhaFuncao(element) {
+        element.classList.toggle("selecionado");
+    }
+    
